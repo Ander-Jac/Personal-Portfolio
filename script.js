691833a6e3d9947a95f52d1b65d4fe454c08c165
Ander-Jac <jajake253@gmail.com>
2022-03-31 21:42:16 -0400

    Added parallax background

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-const openFormButton = document.querySelectorAll("[data-target]")
-const closeFormButton = document.querySelectorAll("[data-close-button]")
-const overlay = document.getElementById("overlay")
+const openFormButton = document.querySelectorAll('[data-target]')
+const closeFormButton = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+const backgroundParallax = document.getElementById('bod')
+const headingParallax = document.getElementById('intro')
+
+window.addEventListener('scroll', function() {
+    let offset = window.pageYOffset;
+    let introOffset = window.pageYOffset - 250;
+    console.log(offset)
+    backgroundParallax.style.backgroundPositionY = (offset * .5 + 'px');
+    
+    if(offset >= 250) {
+        headingParallax.style.left = ( 275 +introOffset * 6 + 'px')
+    } 
+    else headingParallax.style.left = 275 + 'px'
+})
 
 openFormButton.forEach(button => {
-    button.addEventListener("click", () => {
+    button.addEventListener('click', () => {
         const form = document.querySelector(button.dataset.target)
         openForm(form)
     })
 })
 closeFormButton.forEach(button => {
-    button.addEventListener("click", () => {
-        const form = button.closest("#contact-pop")
+    button.addEventListener('click', () => {
+        const form = button.closest('#contact-pop')
         closeForm(form)
     })
 })
 function openForm(form) {
     if (form == null) return
-    form.classList.add("active")
-    overlay.classList.add("active")
+    form.classList.add('active')
+    overlay.classList.add('active')
 }
 function closeForm(form) {
     if (form == null) return
-    form.classList.remove("active")
-    overlay.classList.remove("active")
+    form.classList.remove('active')
+    overlay.classList.remove('active')
 }
